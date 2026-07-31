88f233bd2fc0f626f2dd1a3232e2c06f34632915
utsavgorvadiya148654-coder <utsav.gorvadiya148654@marwadiuniversity.ac.in>
2026-07-31 11:11:14 +0530

    Add files via upload

---
+++ U2-5.C
@@ -0,0 +1,20 @@
+# include<stdio.h>
+# include<conio.h>
+void main()
+{
+	int a,b,c;
+	clrscr();
+	printf("\n enter value of a:");
+	scanf("%d",&a);
+
+	printf("\n enter value of b:");
+	scanf("%d",&b);
+
+	a=a*b;
+
+	printf("\n area of rectangle is :%d",a);
+
+	getch();
+
+}
+
---
+++ U2-6.C
@@ -0,0 +1,15 @@
+//wap which cal vag
+# include<stdio.h>
+# include<conio.h>
+void main()
+{
+	int x,y,z,avg;
+	clrscr();
+	printf("\n enter valueof x,y,z:");
+	scanf("%d%d%d", &x,&y,&z);
+
+	avg=(x+y+z)/3;
+	printf("\n average is : %d",avg);
+	gatdh();
+
+}
