0e50819a0074d1c2323c3da61a6cd9fa2ee2641d
utsavgorvadiya148654-coder <utsav.gorvadiya148654@marwadiuniversity.ac.in>
2026-09-11 10:11:20 +0530

    Add files via upload

---
+++ SYMBOLIC.C
@@ -0,0 +1,17 @@
+//symbolic constants
+#include<stdio.h>
+#include<conio.h>
+
+#define pl 3.14
+#define pf printf
+
+void main()
+{
+	float r=2.5,a;
+	clrscr();
+	a=pl*r*r;
+	printf("%f",a);
+
+	getch();
+
+}
